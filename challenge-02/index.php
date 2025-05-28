af7c5974f1bf1ea05a929ad7798681df6d70ad08
Daniel151505 <daniel150520011@gmail.com>
2025-05-27 22:47:46 -0500

    feat: Challenge 2

--- challenge-02/index.php
+++ challenge-02/index.php
@@ -2,8 +2,47 @@
 
 function noIterate($strArr)
 {
-    // code goes here
-    return $strArr;
+    [$N, $K] = $strArr;
+
+    $kFreq = [];
+    for ($i = 0; $i < strlen($K); $i++) {
+        $char = $K[$i];
+        $kFreq[$char] = ($kFreq[$char] ?? 0) + 1;
+    }
+
+    $required = count($kFreq);
+    $formed = 0;
+    $windowCounts = [];
+
+    $left = 0;
+    $minLen = PHP_INT_MAX;
+    $minWindow = "";
+
+    for ($right = 0; $right < strlen($N); $right++) {
+        $char = $N[$right];
+        $windowCounts[$char] = ($windowCounts[$char] ?? 0) + 1;
+
+        if (isset($kFreq[$char]) && $windowCounts[$char] === $kFreq[$char]) {
+            $formed++;
+        }
+
+        while ($left <= $right && $formed === $required) {
+            if (($right - $left + 1) < $minLen) {
+                $minLen = $right - $left + 1;
+                $minWindow = substr($N, $left, $minLen);
+            }
+
+            $leftChar = $N[$left];
+            $windowCounts[$leftChar]--;
+            if (isset($kFreq[$leftChar]) && $windowCounts[$leftChar] < $kFreq[$leftChar]) {
+                $formed--;
+            }
+
+            $left++;
+        }
+    }
+
+    return $minWindow;
 }
 
 // keep this function call here
